70c1b3a7d76a3f333ae1083cbad9d67e3d8c8f76
Vikram Saini <vikramjit.personal@gmail.com>
2023-11-06 17:31:49 +0530

    API working fine

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
+import { useState } from "react";
 import "./App.css";
 
+// `https://api.frankfurter.app/latest?amount=${}&from=${}&to=${}`
+
 function App() {
+  const [amount, setAmount] = useState("");
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
+  const currFrom = "USD";
+  const currTo = "INR";
+  const currAmount = 100;
+
+  function callApi() {
+    fetch(`https://api.frankfurter.app/latest?amount=100&from=USD&to=INR`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+  callApi();
+
   return (
     <>
-      <input type="NUMBER" />
+      <input type="NUMBER" placeholder="Enter amount here..." />
       <select>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
